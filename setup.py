827dacaf9f43f3e70e7c1e07195d868a8cbe326a
jkswinn <jakeswinneyy@gmail.com>
2023-08-28 19:38:56 +0100

    [BUG] Fix setup arg

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ Saving 12 seconds by spending 90 seconds on a script :D
 import os
 import subprocess
 import json
+import warnings
 
 data_path = "glicko_bot/data"
 data_stores = {"exchange_history.json": {},
@@ -14,14 +15,23 @@ data_stores = {"exchange_history.json": {},
 
 
 if __name__ == "__main__":
+    
+    subprocess.run(["pip", "install", "-r", "requirements.txt"])
 
-    subprocess.run("pip install -r requirements.txt")
+    # create .env file
+    if not os.path.exists(".env"):
+        with open(".env", "w") as f:
+            f.write("DISCORD=YOUR_API_KEY\nRIOT=YOUR_API_KEY\nSUMMONERS=[[`ABC`, `RandomExampleName`, `tft`]]")
+        warnings.warn("Add the appropriate API keys to the newly created .env file.")
+        
+
     if not os.path.exists(data_path):
         print("Creating data directory...")
         os.mkdir(data_path)
         print("Done")
     else:
         print("Data directory already exists.")
+
 
     ap = os.path.join(data_path, "art")
     if not os.path.exists(ap):
