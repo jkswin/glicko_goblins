c430d6fe1af4854c465d69f2664482dc27b7b83b
jkswinn <jakeswinneyy@gmail.com>
2023-08-30 13:30:26 +0100

    [DEV] Update setup.py for new tax system

--- setup.py
+++ setup.py
@@ -10,19 +10,24 @@ import warnings
 data_path = "glicko_bot/data"
 data_stores = {"exchange_history.json": {},
                "exchange.json": {"GLD":0, "SRC":0, "GRC":1},
-                "users.json": {}
+                "users.json": {},
+                "kitty.json": {"tax":1000}
                 }
 
 
 if __name__ == "__main__":
     
-    subprocess.run(["pip", "install", "-r", "requirements.txt"])
+    try:
+        subprocess.run(["pip", "install", "-r", "requirements.txt"])
+    except Exception as e:
+        print(e)
+        print("Requirements will need installing manually.")
 
     # create .env file
     if not os.path.exists(".env"):
         with open(".env", "w") as f:
-            f.write("DISCORD=YOUR_API_KEY\nRIOT=YOUR_API_KEY\nSUMMONERS=[[`ABC`, `RandomExampleName`, `tft`]]")
-        warnings.warn("Add the appropriate API keys to the newly created .env file.")
+            f.write("DISCORD=YOUR_API_KEY\nRIOT=YOUR_API_KEY\nSUMMONERS=[[`GRC`, `RandomExampleName`, `tft`], ['SRC', `OtherExample`, `lol`]]")
+        warnings.warn("Add the appropriate API keys and Summoners to the newly created .env file.")
         
 
     if not os.path.exists(data_path):
@@ -36,14 +41,24 @@ if __name__ == "__main__":
     ap = os.path.join(data_path, "art")
     if not os.path.exists(ap):
         os.mkdir(ap)
-        
+        first_collection_path = os.path.join(ap, "founding_collection")
+        os.mkdir(first_collection_path)
+        with open(os.path.join(first_collection_path, "metadata.jsonl"), "w") as f:
+            json.dump({"name": "EXAMPLE", "base_price": 100, "path": "EXAMPLE.png", "owner": "", "uid": 1, "for_sale": 1, "sale_history": []}, f)
+            f.write("\n")
+        print(f"Succesfully created {first_collection_path}! Time to add art and metadata!")
+    else:
+        print(f"{ap} already exists!")
     
     for filename, content in data_stores.items():
         path = os.path.join(data_path, filename)
         if not os.path.exists(path):
             print(f"Creating {path}")
             with open(path, "w") as f:
-                json.dump(content, f)
+                if filename.endswith(".json"):
+                    json.dump(content, f)
+                else:
+                    f.write(content)
             print("Done")
         else:
             print(f"{path} already exists.")
